1d5467af4a3c37503bfbf08c50dd3ba844354fd0
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-03 17:35:50 +0530

    Gmail like login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <div className="form-container">
+      <img
+          src="https://media.discordapp.net/attachments/852241547327307786/1071035284017381496/logo-gmail-9989.jpg?width=410&height=410" alt="Gmail logo" className="logo" />
+        <form onSubmit={handleSubmit}>
+          <h1>Sign in</h1>
+          <div className="form-group">
+            <label htmlFor="email">Email or phone</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="form-control"
+            />
+            <a href="nothing" className="forgot-password">
+              Forgot password?
+            </a>
+          </div>
+          <button className="sign-in-btn">Sign In</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Login;
